170b5af1f83ae990842a50ac282a235bdc3bb0e0
Nguyễn Thu Thảo Châu <thaochau2201@gmail.com>
2022-09-20 23:16:40 +0700

    Update clients_info.models.js

--- backend/models/clients_info.models.js
+++ backend/models/clients_info.models.js
@@ -6,7 +6,11 @@ export default{
     },
     async findById(id){
         const res = await db("clients_info").where("id",id);
-        return res[0]
+        return res[0];
+    },
+    async findByEmail(email){
+        const res = await db("clients_info").where("email",email);
+        return res[0];
     },
     add(entity){
         return db("clients_info").insert(entity);
